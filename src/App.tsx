0c9fbdfa402a8e53fa718f4ee31a56c58b6ba935
Alonso VN <alonsonh94@gmail.com>
2025-07-22 21:21:51 -0600

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank"></a>
+      </div>
+      <h1>Template Web React</h1>
+      <p> This is a base web react</p>
+    </>
+  );
+}
+
+export default App;
